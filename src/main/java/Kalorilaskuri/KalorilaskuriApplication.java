3749e5d78f9c016443d66d5881e65e05d2a80c4c
MiikkaSa <miikka148@gmail.com>
2024-01-30 09:32:57 +0200

    first commit

---
+++ src/main/java/Kalorilaskuri/KalorilaskuriApplication.java
@@ -0,0 +1,13 @@
+package Kalorilaskuri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KalorilaskuriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KalorilaskuriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Kalorilaskuri/KalorilaskuriApplicationTests.java
@@ -0,0 +1,13 @@
+package Kalorilaskuri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KalorilaskuriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
