67ef93613364c16ea4ca3fe4189df3116f01541a
MiikkaSa <miikka148@gmail.com>
2024-02-06 10:00:51 +0200

    controlleri

--- src/main/java/Kalorilaskuri/Web/KaloriController.java
+++ src/main/java/Kalorilaskuri/Web/KaloriController.java
@@ -2,12 +2,22 @@ package Kalorilaskuri.Web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import Kalorilaskuri.Domain.FoodRepository;
+import Kalorilaskuri.Domain.Food;
 
 @Controller
 public class KaloriController {
     
     @Autowired
     private FoodRepository foodRepository;
+
+    @RequestMapping(value = "/foodList", method = RequestMethod.GET)
+    public String listFoods(Model model) {
+        model.addAttribute("foods", foodRepository.findAll());
+        return "foodList";
+    }
 }
