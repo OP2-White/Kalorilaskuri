4d01721b0c0dc4801427f1ebab25b8ca3c51a2d7
Aapo Rissanen <aapo.rissanen@myy.haaga-helia.fi>
2024-03-05 12:44:52 +0200

    deleted spring security dependencies

--- src/main/java/Kalorilaskuri/Web/KaloriController.java
+++ src/main/java/Kalorilaskuri/Web/KaloriController.java
@@ -3,6 +3,7 @@ package Kalorilaskuri.Web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,11 @@ public class KaloriController {
     
     @Autowired
     private FoodRepository foodRepository;
+
+    @GetMapping("/")
+    public String home() {
+        return "foodListRest"; 
+    }
 
     @CrossOrigin
     @RequestMapping(value = "/foodListRest", method = RequestMethod.GET)
