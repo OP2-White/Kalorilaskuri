cdf827fb1806fd08a9cea239a3954ee68f7e0ad9
Aapo Rissanen <aapo.rissanen@myy.haaga-helia.fi>
2024-03-11 23:44:26 +0200

    changed crossOrigin to whole classes annotation

--- src/main/java/Kalorilaskuri/Web/KaloriController.java
+++ src/main/java/Kalorilaskuri/Web/KaloriController.java
@@ -21,6 +21,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Controller
+@CrossOrigin(origins = "http://localhost:5173")
 public class KaloriController {
     
     @Autowired
@@ -35,19 +36,19 @@ public class KaloriController {
         return "foodListRest"; 
     }
 
-    @CrossOrigin(origins = "http://localhost:5173")
+   
     @RequestMapping(value = "/foodListRest", method = RequestMethod.GET)
     public @ResponseBody List <Food> foodListRest() {
         return(List<Food>) foodRepository.findAll();
     }
 
-    @CrossOrigin(origins = "http://localhost:5173")
+ 
     @RequestMapping(value="foodREST/{id}", method = RequestMethod.GET)
     public @ResponseBody Optional<Food> foodByIdRest(@PathVariable Long id) {
         return foodRepository.findById(id);
     }
 
-    @CrossOrigin(origins = "http://localhost:5173")
+  
     @RequestMapping(value = "/saveFoodREST", method = RequestMethod.POST)
     public ResponseEntity<String> saveFoodRest(@RequestBody Food food) {
         try {
@@ -66,7 +67,7 @@ public class KaloriController {
         }
     }
 
-    @CrossOrigin(origins = "http://localhost:5173")
+
     @RequestMapping(value="saveFoodEatenREST", method = RequestMethod.POST)
     public @ResponseBody FoodEaten saveFoodEatenRest(@RequestBody FoodEaten foodEaten) {
         return foodEatenRepository.save(foodEaten);
