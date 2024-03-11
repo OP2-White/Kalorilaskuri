5ddee42aabccf1afe5e0b4a12d506dd424038b5f
Aapo Rissanen <aapo.rissanen@myy.haaga-helia.fi>
2024-03-11 23:21:51 +0200

    created FoodEatenRepository again because typo in name which didint get fixed in the previous push

---
+++ src/main/java/Kalorilaskuri/Domain/FoodEatenRepository.java
@@ -0,0 +1,10 @@
+package Kalorilaskuri.Domain;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface FoodEatenRepository extends JpaRepository<FoodEaten, Long> {
+    Optional<FoodEaten> findById(Long id);
+    FoodEaten save(FoodEaten foodEaten);
+}
